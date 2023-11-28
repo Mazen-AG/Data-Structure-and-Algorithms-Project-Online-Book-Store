4bb4c042697fd3fb202c86e1d230ad8d3f2927aa
Trimbex <111075379+Trimbex@users.noreply.github.com>
2023-11-28 18:01:28 +0200

    Binary Tree added

---
+++ Online Book Store/Online Book Store/BinaryTree.h
@@ -0,0 +1,30 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+using namespace std;
+template <typename T>
+class BinNode
+{
+public:
+	T data;
+	BinNode<T>* left;
+	BinNode<T>* right;
+	BinNode();
+	BinNode(T item);
+
+};
+template <typename T>
+class BinaryTree
+{
+public:
+	BinNode<T>* root;
+	BinaryTree();
+	bool search(const T& data) const;
+	void insert(const T &data);
+	void deleteNode(T data);
+	BinNode<T>* findMin(BinNode<T>* node) const;
+	void inOrderTraversal(ostream& out, BinNode<T>* node);
+	ostream& operator<<(std::ostream& out, const BinaryTree<T>& tree);
+	
+	
+};
+#endif
